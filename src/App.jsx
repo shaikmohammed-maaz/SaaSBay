263ab2b9b3424f8c4a5ba9b668d0262dd0913482
shaikmohammed-maaz <shaikmaaz333@gmail.com>
2025-07-01 21:08:40 +0530

    added routes to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
 import Navbar from "./components/Navbar";
 import Home from './Home';
 import Footer from './components/Footer';
+// Import other pages as needed
+// import About from './About';
+// import Blog from './Blog';
+// import Categories from './Categories';
+// import ListProducts from './ListProducts';
 
 function App() {
-
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/blog" element={<Blog />} /> */}
+        {/* <Route path="/categories" element={<Categories />} /> */}
+        {/* <Route path="/list-your-products" element={<ListProducts />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
